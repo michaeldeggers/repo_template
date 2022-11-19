b189b511a27616523263fd33a0450f679e999cb9
Michael Eggers <michael.d.eggers@gmail.com>
2022-11-19 18:48:20 -0500

    update template to  use vars

--- providers.tf
+++ providers.tf
@@ -1,5 +1,12 @@
 # Using a single workspace:
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.40.0"
+    }
+  }
+  required_version = ">= 1.1.0"
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "eggs-projects"
@@ -7,5 +14,14 @@ terraform {
     workspaces {
       prefix = "eggs-projects-"
     }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::${var.aws_account_id}:role/eggs-projects-${var.project_name}-role"
+    session_name = "Session_GitHub_Actions"
   }
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_account_id" {
+  description = "AWS Account ID"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project Name"
+  type        = string
+}
